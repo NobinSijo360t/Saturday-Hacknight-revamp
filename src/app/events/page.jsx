cd480ed267339e27148c6bb7d6251ed86c53bd93
Nobin <143606368+NobinSijo360t@users.noreply.github.com>
2024-09-17 22:34:16 +0530

    Updated src files

---
+++ src/app/events/page.jsx
@@ -0,0 +1,91 @@
+"use client"
+
+import React from 'react'
+
+import calendarLogo from "/public/assets/images/calendar_logo.svg"
+import streamlit from "/public/assets/images/streamlit_main.svg"
+
+import "./page.css"
+
+
+import EventCard from '@/components/eventComponents/EventCard'
+import Timer from '@/components/Timer'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
+// import Timer from '@/components/eventComponents/Timer'
+
+export default function EventsSection() {
+
+
+  const events = [1,2,3,4,5,6,7,8];
+
+  return (
+    <div className='eventsPage'>
+
+
+      <Navbar/>
+
+      <div className='events_page_blur events_blur1'></div>
+      <div className='events_page_blur events_blur2'></div>
+      {/* <div className='events_page_blur events_blur3'></div> */}
+
+      <div className="hero">
+
+        <div className="HerotitleSection">
+          <h3>Saturday HackNight <span>this week</span></h3>
+        </div>
+
+        <div className="HeroBottomSection">
+          
+          <div className="HeroLeft">
+          <Timer/>
+            <div className="eventDetails">
+              <div className="eventTitleLeft">Streamlit</div>
+              <div className="eventDate"><img src={calendarLogo.src} alt="" /> <span>Sat May 18 2024</span> </div>
+              <div className="leftButtons">
+                <div className="registerButton">REGISTER</div>
+                <div className="infoButton">More Info</div>
+              </div>
+
+            </div>
+          </div>
+
+          <div className="HeroRight">
+
+            <img src={streamlit.src} alt="" />
+            <p>streamlit is an open-source python framework to deliver dynamic data with only a few lines of code</p>
+          </div>
+
+
+
+
+        </div>
+
+
+
+      </div>
+      <div className="eventCardSection">
+      <h3 className = "eventCardSectionHeading">EXPOLORED AREAS</h3>
+      <div className="eventCards">
+
+
+      {
+        events.map((index)=>{
+          return(
+            <>
+            <div key = {index}><EventCard /></div>
+            
+            </>
+          )
+        })
+      }
+
+      </div>
+
+      </div>
+
+      <Footer/>
+
+    </div>
+  )
+}
